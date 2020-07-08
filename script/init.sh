11d70285a64b6a6e3a4af6105a369848acc6270e
kukaro <justkukaro@naver.com>
2020-07-08 15:47:14 +0900

    스크립트 도커 컴포즈 추가

--- script/init.sh
+++ script/init.sh
@@ -1,8 +1,16 @@
 is_docker_exist=$(which docker | wc -c)
 if [ "$is_docker_exist" -gt 0 ];then
     echo "docker exist"
-el
-    echo "docker don't exist"
+else
+    echo "docker dosen't exist"
     yum install -y dockeryum install -y dockeryum install -y docker
 fi
-chmod -R 777 /var/run/docker.sock
+systemctl start docker.service
+is_docker_compose_exist=$(which docker-compose | wc -c)
+if [ "$is_docker_compose_exist" -gt 0 ];then
+    echo "docker-compose exist"
+else
+    echo "docker-compose dosen't exist"
+    curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+fi
+chmod -R 777 /usr/local/bin/docker-compose
