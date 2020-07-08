ad0abe3e137c1f24a66f57a31704e4de9f4e7b27
kukaro <kukaro4515@gmail.com>
2020-07-08 15:03:56 +0900

    remove line end

--- script/init.sh
+++ script/init.sh
@@ -2,6 +2,7 @@ is_docker_exist=`which docker | wc -c`
 if [ $is_docker_exist -gt 0 ];then
     echo "docker exist"
 else
+    echo "docker don't exist"
     yum install -y dockeryum install -y dockeryum install -y docker
 if
 chmod -R 777 /var/run/docker.sock
