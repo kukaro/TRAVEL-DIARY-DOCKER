b99e19e7892c3451be7f5560ee28971fbe84b11a
kukaro <justkukaro@naver.com>
2020-07-08 15:00:44 +0900

    스크립트 추가

---
+++ script/init.sh
@@ -0,0 +1,12 @@
+is_docker_exist=`which docker | wc -c`
+if [ $is_docker_exist -gt 0 ];then
+    echo "docker exist"
+else
+    yum install -y dockeryum install -y dockeryum install -y docker
+if
+chmod -R 777 /var/run/docker.sock
+
+
+
+
+
