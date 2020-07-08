b54e847ef1f680ad085871c3efad26c2723bb8c2
kukaro <kukaro4515@gmail.com>
2020-07-08 15:36:51 +0900

    shell script change

--- script/init.sh
+++ script/init.sh
@@ -1,8 +1,8 @@
-is_docker_exist=`which docker | wc -c`
-if [ $is_docker_exist -gt 0 ];then
+is_docker_exist=$(which docker | wc -c)
+if [ "$is_docker_exist" -gt 0 ];then
     echo "docker exist"
-else
+el
     echo "docker don't exist"
     yum install -y dockeryum install -y dockeryum install -y docker
-if
+fi
 chmod -R 777 /var/run/docker.sock
