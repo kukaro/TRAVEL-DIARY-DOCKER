27c976ca595e160972d588a60ca939db93f9d498
kukaro <justkukaro@naver.com>
2020-07-09 10:12:54 +0900

    api서버 빌드내용 추가

--- script/init.sh
+++ script/init.sh
@@ -28,8 +28,8 @@ fi
 
 chmod -R 777 ../config
 
-host_ips=$(hostname -I)
-host_ips=$(echo "$host_ips" | tr " " "\n" | head -1)
-echo "$host_ips" > ../config/host_ip.txt
+host_ip=$(hostname -I)
+host_ip=$(echo "$host_ip" | tr " " "\n" | head -1)
+echo "$host_ip" > ../config/host_ip.txt
 
-cd ../ && docker-compose up -d
+cd ../ && HOST_IP=${host_ip} docker-compose up -d
