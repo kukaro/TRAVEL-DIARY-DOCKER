de0d4edbbb70defac90065f1f19ad1a3bcad956e
kukaro <justkukaro@naver.com>
2020-07-09 14:42:01 +0900

    add submodule

--- script/env_remove.sh
+++ script/env_remove.sh
@@ -3,14 +3,10 @@ cd "$RELATIVE_DIR"
 SHELL_PATH=$(pwd -P)
 cd "$SHELL_PATH"
 cd ../TRAVEL-DIARY-API-SERVER/project/src/project
-composer install
 
 if [ -f ".env" ];then
     echo "env file exist"
+    rm .env
 else
     echo "env file doesn't exist"
-    cp .env.example .env
 fi
-
-chmod -R 777 . 
-php artisan key:generate
