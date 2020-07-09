46d42ae729620202bf2330b422e28b3d99ed9767
kukaro <justkukaro@naver.com>
2020-07-09 13:53:17 +0900

    key generation

--- script/run_time.sh
+++ script/run_time.sh
@@ -4,3 +4,13 @@ SHELL_PATH=$(pwd -P)
 cd "$SHELL_PATH"
 cd ../TRAVEL-DIARY-API-SERVER/project/src/project
 composer install
+
+if [ -f ".env" ];then
+    echo "env file exist"
+else
+    echo "env file doesn't exist"
+    cp .env.example .env
+fi
+
+chmod -R 777 storage/logs
+php artisan key:generate
